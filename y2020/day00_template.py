a714a2d11553e875b384462cef4e90642d61d507
Tomáš Kubín <cathack@gmail.com>
2020-12-02 09:48:19 +0100

    2020/02

---
+++ y2020/day00_template.py
@@ -0,0 +1,28 @@
+"""
+Advent of Code 2020
+Day X: title
+https://adventofcode.com/2020/day/X
+"""
+
+
+def part_1():
+    """
+    Instructions for part 1.
+    """
+    result = 1
+    print(f"part 1: {result}")
+    return result
+
+
+def part_2():
+    """
+    Instructions for part 2.
+    """
+    result = 2
+    print(f"part 2: {result}")
+    return result
+
+
+if __name__ == '__main__':
+    part_1()
+    part_2()
---
+++ y2020/day02_passwords.py
@@ -0,0 +1,166 @@
+"""
+Advent of Code 2020
+Day 2: Password Philosophy
+https://adventofcode.com/2020/day/2
+"""
+
+from typing import Iterable
+from typing import List
+from typing import Tuple
+
+from utils import parse_line
+
+
+def part_1(rules_passwords: List[Tuple['PasswordRule', str]]) -> int:
+    """
+    Suppose you have the following list:
+
+        1-3 a: abcde
+        1-3 b: cdefg
+        2-9 c: ccccccccc
+
+    Each line gives the password policy and then the password. The password policy indicates
+    the lowest and highest number of times a given letter must appear for the password to be valid.
+    For example, `1-3 a` means that the password must contain `a` at least 1 time and at most
+    3 times.
+
+    In the above example, 2 passwords are valid. The middle password, `cdefg`, is not;
+    it contains no instances of `b`, but needs at least 1.
+
+        >>> PasswordRule(1, 3, 'b').is_valid_1('cdefg')
+        False
+
+    The first and third passwords are valid: they contain one `a` or nine `c`, both within
+    the limits of their respective policies.
+
+        >>> PasswordRule(1, 3, 'a').is_valid_1('abcde')
+        True
+        >>> PasswordRule(2, 9, 'c').is_valid_1('ccccccccc')
+        True
+
+    How many passwords are valid according to their policies?
+
+        >>> part_1([
+        ...     (PasswordRule(1, 3, 'a'), 'abcde'),
+        ...     (PasswordRule(1, 3, 'b'), 'cdefg'),
+        ...     (PasswordRule(2, 9, 'c'), 'ccccccccc')
+        ... ])
+        part 1: 2/3 passwords are valid
+        2
+    """
+    result = sum(1 for rule, password in rules_passwords if rule.is_valid_1(password))
+    print(f"part 1: {result}/{len(rules_passwords)} passwords are valid")
+    return result
+
+
+def part_2(rules_passwords: List[Tuple['PasswordRule', str]]) -> int:
+    """
+    Each policy actually describes two positions in the password, where 1 means the first
+    character, 2 means the second character, and so on. *Exactly one* of these positions must
+    contain the given letter. Other occurrences of the letter are irrelevant for the purposes
+    of policy enforcement.
+
+    Given the same example list from above:
+
+    - `1-3 a: abcde` is *valid*: position 1 contains `a` and position 3 does not.
+
+        >>> PasswordRule(1, 3, 'a').is_valid_2('abcde')
+        True
+
+    - `1-3 b: cdefg` is *invalid*: neither position 1 nor position 3 contains `b`.
+
+        >>> PasswordRule(1, 3, 'b').is_valid_2('cdefg')
+        False
+
+    - `2-9 c: ccccccccc` is *invalid*: both position 2 and position 9 contain `c`.
+
+        >>> PasswordRule(2, 9, 'c').is_valid_2('ccccccccc')
+        False
+
+    How many passwords are valid according to the new interpretation of the policies?
+
+        >>> part_2([
+        ...     (PasswordRule(1, 3, 'a'), 'abcde'),
+        ...     (PasswordRule(1, 3, 'b'), 'cdefg'),
+        ...     (PasswordRule(2, 9, 'c'), 'ccccccccc')
+        ... ])
+        part 2: 1/3 passwords are valid
+        1
+    """
+    result = sum(1 for rule, password in rules_passwords if rule.is_valid_2(password))
+    print(f"part 2: {result}/{len(rules_passwords)} passwords are valid")
+    return result
+
+
+class PasswordRule:
+    def __init__(self, min_count: int, max_count: int, character: str):
+        assert 1 <= min_count < max_count
+        assert len(character) == 1
+
+        self.min_count = min_count
+        self.max_count = max_count
+        self.character = character
+
+    def is_valid_1(self, password: str) -> bool:
+        """
+        The lowest and highest number of times a given letter must appear
+        for the password to be valid.
+
+            >>> PasswordRule(2, 3, 'a').is_valid_1('a')
+            False
+            >>> PasswordRule(2, 3, 'a').is_valid_1('aa')
+            True
+            >>> PasswordRule(2, 3, 'a').is_valid_1('aaa')
+            True
+            >>> PasswordRule(2, 3, 'a').is_valid_1('aaaa')
+            False
+            >>> PasswordRule(2, 3, 'a').is_valid_1('bcd')
+            False
+        """
+        ch_count = sum(1 for ch in password if ch == self.character)
+        return self.min_count <= ch_count <= self.max_count
+
+    def is_valid_2(self, password: str) -> bool:
+        """
+        Exactly one of these positions must contain the given letter.
+
+            >>> PasswordRule(1, 4, 'a').is_valid_2('bbbb')
+            False
+            >>> PasswordRule(1, 4, 'a').is_valid_2('abbb')
+            True
+            >>> PasswordRule(1, 4, 'a').is_valid_2('bbba')
+            True
+            >>> PasswordRule(1, 4, 'a').is_valid_2('abba')
+            False
+
+            >>> PasswordRule(1, 4, 'x').is_valid_2('x')
+            Traceback (most recent call last):
+            ...
+            AssertionError
+        """
+        pos1, pos2 = self.min_count - 1, self.max_count - 1
+        assert 0 <= pos1 < pos2 < len(password)
+        found_count = sum(1 for pos in (pos1, pos2) if password[pos] == self.character)
+        return found_count == 1
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.min_count!r}, {self.max_count!r}, {self.character!r})'
+
+    def __str__(self):
+        return f"{self.min_count}-{self.max_count} {self.character}"
+
+
+def load_data(fn: str) -> Iterable[Tuple[PasswordRule, str]]:
+    with open(fn) as f:
+        for line in f:
+            # 1-3 b: cdefg
+            min_count, max_count, character, password = parse_line(line, "", "-", " ", ": ", "\n")
+            yield PasswordRule(int(min_count), int(max_count), character), password
+
+
+if __name__ == '__main__':
+    data = list(load_data('data/02-input.txt'))
+    assert len(data) == 1000
+
+    part_1(data)
+    part_2(data)
